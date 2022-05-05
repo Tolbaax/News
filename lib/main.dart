3805646f72afedd5b1ded789c51ab74af7c4b9d2
Mohamed Tolba <Tolbaax@gmail.com>
2022-05-05 08:53:02 +0200

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:news365/shared/cubit/cubit.dart';
 import 'package:news365/shared/cubit/states.dart';
 import 'package:news365/shared/network/local/cache_helper.dart';
 import 'package:news365/shared/network/remote/dio_helper.dart';
+import 'package:news365/shared/styles/theme.dart';
 import 'layout/home_screen.dart';
 
 void main() async
@@ -39,76 +38,8 @@ class MyApp extends StatelessWidget {
         builder: (context, state) {
           return  MaterialApp(
             debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primarySwatch: Colors.deepOrange,
-              appBarTheme: const AppBarTheme(
-                titleSpacing: 20.0,
-                systemOverlayStyle: SystemUiOverlayStyle(
-                  statusBarColor: Colors.grey,
-                  statusBarBrightness: Brightness.dark,
-                ),
-                titleTextStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                iconTheme: IconThemeData(
-                  color: Colors.black,
-                ),
-                backgroundColor: Colors.white,
-                elevation: 0.0,
-              ),
-              bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                  selectedItemColor: Colors.deepOrange,
-                  type: BottomNavigationBarType.fixed,
-                  backgroundColor: Colors.white,
-                  elevation: 20.0
-              ),
-              textTheme: const TextTheme(
-                  bodyText1: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black
-                  )
-              ),
-              scaffoldBackgroundColor: Colors.white,
-            ),
-
-            darkTheme: ThemeData(
-              primarySwatch: Colors.deepOrange,
-              appBarTheme: AppBarTheme(
-                titleSpacing: 20.0,
-                systemOverlayStyle: SystemUiOverlayStyle(
-                  statusBarColor: HexColor('333739'),
-                  statusBarBrightness: Brightness.light,
-                ),
-                titleTextStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                iconTheme: const IconThemeData(
-                  color: Colors.white,
-                ),
-                backgroundColor:HexColor('333739'),
-                elevation: 0.0,
-              ),
-              bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                  selectedItemColor: Colors.deepOrange,
-                  unselectedItemColor: Colors.grey,
-                  type: BottomNavigationBarType.fixed,
-                  backgroundColor: HexColor('333739'),
-                  elevation: 20.0
-              ),
-              textTheme: const TextTheme(
-                  bodyText1: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white
-                  )
-              ),
-              scaffoldBackgroundColor: HexColor('333739'),
-            ),
+            theme: light,
+            darkTheme: dark,
             themeMode: NewsCubit.get(context).isDark? ThemeMode.dark : ThemeMode.light,
             home: const HomeScreen(),
           );
---
+++ lib/shared/styles/theme.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+ThemeData light = ThemeData(
+  primarySwatch: Colors.deepOrange,
+  appBarTheme: const AppBarTheme(
+    titleSpacing: 20.0,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: Colors.grey,
+      statusBarBrightness: Brightness.dark,
+    ),
+    titleTextStyle: TextStyle(
+      color: Colors.black,
+      fontSize: 25.0,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.black,
+    ),
+    backgroundColor: Colors.white,
+    elevation: 0.0,
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Colors.deepOrange,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.white,
+      elevation: 20.0
+  ),
+  textTheme: const TextTheme(
+      bodyText1: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w600,
+          color: Colors.black
+      )
+  ),
+  scaffoldBackgroundColor: Colors.white,
+);
+
+ThemeData dark = ThemeData(
+  primarySwatch: Colors.deepOrange,
+  appBarTheme: AppBarTheme(
+    titleSpacing: 20.0,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: HexColor('333739'),
+      statusBarBrightness: Brightness.light,
+    ),
+    titleTextStyle: const TextStyle(
+      color: Colors.white,
+      fontSize: 25.0,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white,
+    ),
+    backgroundColor:HexColor('333739'),
+    elevation: 0.0,
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.deepOrange,
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: HexColor('333739'),
+      elevation: 20.0
+  ),
+  textTheme: const TextTheme(
+      bodyText1: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w600,
+          color: Colors.white
+      )
+  ),
+  scaffoldBackgroundColor: HexColor('333739'),
+);
